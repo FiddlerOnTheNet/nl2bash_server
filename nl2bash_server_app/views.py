3c1960f62a63e6ab815423f10d007676337b88d6
lmartini <lmartini@live.com>
2018-05-05 10:34:02 -0700

    Updating page css

--- nl2bash_server_app/views.py
+++ nl2bash_server_app/views.py
@@ -8,7 +8,7 @@ def get_next_unverified():
     """ Retrieves the next English command from the database
     that has no verified bash commands associated with it. If
     no such English commands exist, it returns None for now. """
-    return EnglishDescription.objects.filter(num_verified__gt=0).first()
+    return EnglishDescription.objects.filter(num_verified=0).first()
 
 
 def tester(request):
@@ -18,18 +18,22 @@ def tester(request):
 
     # Get the next English command from the database that has
     # no verified bash commands associated with it.
-    eng_cmd = get_next_unverified()
+    eng_cmd = str(get_next_unverified())
 
     # Get the list of command pairs with this english command
     # description
-    cmd_pair_list = CommandPair.objects.filter(nl__exact=eng_cmd)
+    cmd_pair_list = CommandPair.objects.filter(nl__cmd__exact=eng_cmd)[:5]
 
     # Extract up to five possible bash commands to display
+    bash_cmd_list = []
+    for cmd_pair in cmd_pair_list:
+        bash_cmd_list.append(str(cmd_pair.bash))
 
     return render(
         request,
         'tester.html',
-        context={"english_command": eng_cmd, },
+        context={"english_command": eng_cmd,
+                 "bash_cmd_list": bash_cmd_list},
     )
 
 def submit(request):
