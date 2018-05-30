3b018e932f9f3af65155bc5277ea4d3889a6a0ce
lmartini <lmartini@live.com>
2018-05-30 10:54:09 -0700

    Reverting add_data_from_scraper

--- nl2bash_server/add_data_from_scraper.py
+++ nl2bash_server/add_data_from_scraper.py
@@ -4,14 +4,12 @@
 # description.
 # This script takes a directory of those files, then creates CommandPair
 # entries in the database for them.
-# Updated to parse JSON .verify files!
 
 # To add test data:
 # Run with: python -m nl2bash_server.add_data_from_scraper <path to ScrapedPages>
 # In the directory with manage.py (top dir of project)
 
-import sys, os, django
-import json
+import sys, os, django, json
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nl2bash_server.settings")
 django.setup()
@@ -28,12 +26,12 @@ def clean_all():
     Verification.objects.all().delete()
 
 
-# Uncomment to reset database
-# clean_all()
-
 if len(sys.argv) < 1:
     print("Usage: " + sys.argv[0] + " <path to dir with data (.verify) files>")
     sys.exit()
+
+# Uncommented to reset database
+clean_all()
 
 # Get path to data
 file_path = sys.argv[1]
@@ -42,26 +40,42 @@ file_path = sys.argv[1]
 for filename in os.scandir(file_path):
     filename = os.fsdecode(filename)
     if filename.endswith('.verify'):
-        with open(filename) as f:
+        with open(filename, 'rb') as f:
+            # Read f into a string and parse its json
             obj = json.loads(f.read().decode('utf-8', 'ignore'))
 
-            # Extract the english description, remove trailing newline
+            # Extract the english description
             eng_text = obj['title']
             eng_cmd = EnglishDescription(cmd=eng_text)
 
-            # Check if this English command is already in the DB
-            if not EnglishDescription.objects.filter(cmd=eng_text).exists() \
-                    and eng_text is not None:
-                eng_cmd.save()
-                command_list = obj['commands']
-                for bash_text in command_list:
-                    # Make a new CommandPair - this should also add the
-                    # corresponding EnglishDescription and BashCommand
-                    bash_cmd = BashCommand(cmd=bash_text)
-                    if bash_text is not None:
-                        bash_cmd.save()
-                        ver = Verification(score=0)
-                        ver.save()
-                        cmd_pair = CommandPair(nl=eng_cmd, bash=bash_cmd,
-                                               ver_status=ver)
-                        cmd_pair.save()
+            if eng_text is not None:
+                # Check if this English command is already in the DB
+                if not EnglishDescription.objects.filter(cmd=eng_text).exists():
+                    eng_cmd.save()
+
+                    # Commands are in a list now
+                    for bash_text in obj['commands']:
+                        # Make a new CommandPair - this should also add the
+                        # corresponding EnglishDescription and BashCommand
+                        bash_cmd = BashCommand(cmd=bash_text)
+                        if bash_text is not None:
+                            bash_cmd.save()
+                            ver = Verification(score=0)
+                            ver.save()
+                            cmd_pair = CommandPair(nl=eng_cmd, bash=bash_cmd,
+                                                   ver_status=ver)
+                            cmd_pair.save()
+                else:
+                    # If it is in the db, we append command pairs.
+                    for bash_text in obj['commands']:
+                        # Standard procedure, like above.
+                        bash_cmd = BashCommand(cmd=bash_text)
+
+                        # For each command, check if the pair itself is also in.
+                        if bash_text is not None and not CommandPair.objects.filter(nl=eng_cmd, bash=bash_cmd).exists():
+                            bash_cmd.save()
+                            ver = Verification(score=0)
+                            ver.save()
+                            cmd_pair = CommandPair(nl=eng_cmd, bash=bash_cmd,
+                                                   ver_status=ver)
+                            cmd_pair.save()
