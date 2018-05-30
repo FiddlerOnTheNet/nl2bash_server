3e1bc0cad1d922b263f0b1c383895d5ce8954f8d
lmartini <lmartini@live.com>
2018-05-30 00:32:55 -0700

    Updated models.py

--- nl2bash_server_app/models.py
+++ nl2bash_server_app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse
 import uuid
-
+import django.utils.timezone as timezone
+from datetime import timedelta
 
 # Create your models here.
 
@@ -13,10 +14,18 @@ class EnglishDescription(models.Model):
     # score above 0
     num_verified = models.IntegerField('Number of verified command pairs', null=False,
                                        default=0)
+    time_accessed = models.DateTimeField(default=timezone.now)
 
     def inc_num_verified(self):
         """ Add 1 to the number of verified commands with this English description. """
         self.num_verified += 1
+
+    def check_time_threshold(self, thresh):
+        """ Checks if at least thresh minutes have passed since last updating the
+        time accessed. """
+        start_time = self.time_accessed
+        end_time = timezone.now
+        return (end_time - start_time) > timedelta(minutes=thresh)
 
     def __str__(self):
         """ String representation of an EnglishDescription """
@@ -48,7 +57,11 @@ class CommandPair(models.Model):
         """ Mark this command pair as having been saved in all.nl and all.cmd. """
         self.saved_status = 1
 
-    # def get_absolute_url(self):
+    def set_time_accessed(self):
+        """ Sets the timefield to when this method is called. """
+        self.time_accessed = timezone.now()
+
+        # def get_absolute_url(self):
     #     """ Returns the url to access a detailed record for this CommandPair. """
     #     return reverse("command_pair_detail", args=[str(self.id)])
 
