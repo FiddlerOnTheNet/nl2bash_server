af7a1b4a0a43e313a6cfa1740b219b117cb8060a
anabh4569 <anabh4569@gmail.com>
2018-06-02 12:41:46 -0700

    Added another echo to runserver to make steps clear.

--- runserver.sh
+++ runserver.sh
@@ -8,6 +8,7 @@ pipenv run python manage.py migrate
 echo Loading .verify files into database.
 pipenv run python -m nl2bash_server.add_data_from_scraper ./test_pages/ScrapedPages
 pipenv run python manage.py runserver
+echo
 echo Outputting all.cm and all.nl from database.
 pipenv run python -m nl2bash_server.save_data
 touch all.cm
