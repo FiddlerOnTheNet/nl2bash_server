2d0e66ef8cbdd8d7e428982d5cbc403cb7b7c7d7
anabh4569 <anabh4569@gmail.com>
2018-06-02 12:14:29 -0700

    Fixed runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+pip3 install pipenv
+
 # Taken from fullpipeline.sh in the parent repo.
+pipenv install
 pipenv run python manage.py migrate
 pipenv run python -m nl2bash_server.add_data_from_scraper ./test_pages/ScrapedPages
 ((pipenv run python manage.py runserver) > server.output) &
 PID=$!
 read -p "Press enter to stop hosting the tester ui interface and save to all.cm/.nl."
-kill $PID
+kill -SIGINT $PID
 pipenv run python -m nl2bash_server.save_data
 touch all.cm
 touch all.nl
