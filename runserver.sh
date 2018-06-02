8b735bc1c6c9eea268a69d7b6c53581cdae7e1ba
anabh4569 <anabh4569@gmail.com>
2018-06-02 12:40:35 -0700

    Added echoes to runserver to make steps clear.

--- runserver.sh
+++ runserver.sh
@@ -5,8 +5,10 @@ pip3 install pipenv
 # Taken from fullpipeline.sh in the parent repo.
 pipenv install
 pipenv run python manage.py migrate
+echo Loading .verify files into database.
 pipenv run python -m nl2bash_server.add_data_from_scraper ./test_pages/ScrapedPages
 pipenv run python manage.py runserver
+echo Outputting all.cm and all.nl from database.
 pipenv run python -m nl2bash_server.save_data
 touch all.cm
 touch all.nl
